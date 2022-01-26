bc67c75388df2a04c6c6238c5ccbc7c20f23981f
Sean Yang <seanyixuan@gmail.com>
2022-01-26 03:37:29 -0800

    implemented active class to hold selected rating and added disabled class to remove pointer event once rating is selected

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,15 @@
 const stars = document.querySelectorAll(".stars .single-star");
+const star = document.querySelector('.stars');
 console.log(stars);
 
-stars.forEach((star, i) => {
-    star.addEventListener('click', () => {
-        console.log(`Index ${i} star was selected from right to left`);
+stars.forEach((selectedStar, i) => {
+    selectedStar.addEventListener('click', () => {
+        console.log(`Index ${i} star was selected going from right to left`);
+        star.classList.add('disabled');
+        stars.forEach((otherStar, otherIndex) => {
+            if (otherIndex >= i ) {
+            otherStar.classList.add("active");
+            }
+        })
     })
 })
