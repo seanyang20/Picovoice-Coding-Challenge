321642bf2371fac8fcc9d7c073d089670db851fb
Sean Yang <seanyixuan@gmail.com>
2022-01-27 01:09:00 -0800

    created uuid for each product and made axios post call to send info to server side

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,9 +4,28 @@
 
 const starContainers = document.querySelectorAll(".stars");
 
+// function to generate uuid for each product
+function uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+
 starContainers.forEach((el) => {
   const starsUL = createElements(el, "ul", "main");
   const output = createElements(el, "div", "output");
+
+  // obtaining product info
+  const product = el.querySelector('.product');
+  const productName = product.innerHTML;
+  const productId = uuidv4();
+  console.log(product);
+  console.log(productName);
+  console.log(productId);
+ 
+
   // looping to create each individual star
   for (let x = 0; x < 5; x++) {
     const star = createElements(starsUL, "li", "star");
@@ -22,12 +41,28 @@ starContainers.forEach((el) => {
   curStars.forEach((singleStar, Idx) => {
     singleStar.addEventListener("click", () => {
       console.log(`Rating of ${Math.abs(Idx - 5)} was selected`);
+
+      const starValue = Math.abs(Idx - 5);
+      console.log(starValue);
+      console.log(productId);
+      console.log(productName);
+
+      // To send request to web server
+      axios.post( URL, {
+        productId: productId,
+        productName: productName,
+        starRating: starValue
+      })
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });
+
       el.classList.add("no-hover");
       curStars.forEach((otherStar, otherIndex) => {
-      
         if (otherIndex >= Idx) {
           otherStar.classList.add("active");
-
         } else {
           otherStar.classList.remove("active");
         }
