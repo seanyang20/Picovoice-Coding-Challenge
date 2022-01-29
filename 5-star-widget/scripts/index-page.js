5b9aea8a9c63cbf0e2b7817f527dd58cef717d68
Sean Yang <seanyixuan@gmail.com>
2022-01-29 00:48:42 -0800

    improved documentation and organization of widget code

--- 5-star-widget/scripts/index-page.js
+++ 5-star-widget/scripts/index-page.js
@@ -1,35 +1,29 @@
-// const stars = document.querySelectorAll(".stars .single-star");
-// const star = document.querySelector('.stars');
-// console.log(stars);
-
 const starContainers = document.querySelectorAll(".stars");
 
 // function to generate uuid for each product
 function uuidv4() {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-      var r = (Math.random() * 16) | 0,
-        v = c == "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
+// creating content for each star ranking system
 starContainers.forEach((el) => {
   const starsUL = createElements(el, "ul", "main");
-  const output = createElements(el, "div", "output");
 
   // obtaining product info
-  const product = el.querySelector('.product');
+  const product = el.querySelector(".product");
   const productName = product.innerHTML;
   const productId = uuidv4();
   console.log(product);
   console.log(productName);
   console.log(productId);
- 
 
   // looping to create each individual star
   for (let x = 0; x < 5; x++) {
     const star = createElements(starsUL, "li", "star");
-    // star.innerHTML = "&#9734";
     star.starValue = x + 1; // storing the star value for each star rating
     console.log(star);
   }
@@ -38,8 +32,12 @@ starContainers.forEach((el) => {
   const curStars = starsUL.querySelectorAll(".star");
   console.log(curStars);
 
+  // adding event listener click to each star of selected rating system
   curStars.forEach((singleStar, Idx) => {
     singleStar.addEventListener("click", () => {
+      // index direction is reversed for preceding elements to have the same state
+      // as the hovered or clicked star so the below is necessary to have the index
+      // match the actual star number selected
       console.log(`Rating of ${Math.abs(Idx - 5)} was selected`);
 
       const starValue = Math.abs(Idx - 5);
@@ -48,39 +46,34 @@ starContainers.forEach((el) => {
       console.log(productName);
 
       // To send request to web server
-      axios.post( URL, {
-        productId: productId,
-        productName: productName,
-        starRating: starValue
-      })
-      .then((response) => {
-        console.log(response);
-      }, (error) => {
-        console.log(error);
-      });
+      axios
+        .post(URL, {
+          productId: productId,
+          productName: productName,
+          starRating: starValue,
+        })
+        .then(
+          (response) => {
+            console.log(response);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
 
       el.classList.add("no-hover");
+
+      // Enable stars preceding the clicked star to also be in active state
       curStars.forEach((otherStar, otherIndex) => {
         if (otherIndex >= Idx) {
           otherStar.classList.add("active");
         } else {
+          // removes active state of stars succeeding clicked star
           otherStar.classList.remove("active");
         }
       });
     });
   });
-
-  // starsUL.forEach((selectedStar, i) => {
-  //   selectedStar.addEventListener("click", () => {
-  //     console.log(`Index ${i} star was selected going from right to left`);
-  //     star.classList.add("disabled");
-  //     stars.forEach((otherStar, otherIndex) => {
-  //       if (otherIndex >= i) {
-  //         otherStar.classList.add("active");
-  //       }
-  //     });
-  //   });
-  // });
 });
 
 // function to create an element
