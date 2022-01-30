81dd1da3af95de16894b6d9725f16876ab966607
Sean Yang <seanyixuan@gmail.com>
2022-01-30 15:42:09 -0800

    created given function that is to be wrapped

--- get-book-info/get-book-info-via-cache.py
+++ get-book-info/get-book-info-via-cache.py
@@ -1,6 +1,16 @@
 from collections import OrderedDict
+from functools import cache
 
 # REFERENCE 
 # For Python 3, OrderedDict object has the following two functions:
 # move_to_end(key, last=True), it moves the key to the right end (last = True), or left end 
 # (last = False); popitem(last=True) removes item from right end (last = True), or left end (last = False)
+
+cache = OrderedDict()
+
+# Assume this function is given and to be wrapped 
+def get_book_info(isbn):
+    return {"The Last Lecture", "Randy Pausch", "English"}
+
+
+    
