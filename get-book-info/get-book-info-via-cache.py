d263366b07e14d006a94ecc79889f3ee4eb3cf16
Sean Yang <seanyixuan@gmail.com>
2022-01-30 15:38:06 -0800

    starting get-book-info question

---
+++ get-book-info/get-book-info-via-cache.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+
+# REFERENCE 
+# For Python 3, OrderedDict object has the following two functions:
+# move_to_end(key, last=True), it moves the key to the right end (last = True), or left end 
+# (last = False); popitem(last=True) removes item from right end (last = True), or left end (last = False)
