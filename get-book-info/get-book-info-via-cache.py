4b86a659e14fcb38b0c5a59973789eb372de49a1
Sean Yang <seanyixuan@gmail.com>
2022-01-30 17:05:30 -0800

    implemented functionality for getting book object when not in cache

--- get-book-info/get-book-info-via-cache.py
+++ get-book-info/get-book-info-via-cache.py
@@ -17,11 +17,22 @@ def get_book_info_via_cache(isbn):
     # if the requested input exists in the cache then we retrieve it from the cache
     if isbn in cache:
         cache.move_to_end(isbn) # move to last position of LRU for optimization 
-        return cache[isbn]
-
+        return cache[isbn]  
+    else: 
+        book = get_book_info(isbn) # call the function to get book info if not cached 
+        cache[isbn] = book         # caching book info
+        cache.move_to_end(isbn)    # moe to end of cache 
+    
+        
 
 # Testing 
 get_book_info("1")
 get_book_info_via_cache("1")
 print(cache)     
 
+get_book_info_via_cache("2")
+print(cache)
+get_book_info_via_cache("3")
+print(cache)
+get_book_info_via_cache("4")
+print(cache)
